540f1b0c5c438481b523e878c3100b51cfe4d4c7
Marcus Nogueira <marcusnogueira2105@gmail.com>
2026-09-08 10:29:09 -0300

    feat(security): adicionar regra de ingress para acesso público na porta 3001

--- infra/homol.tf
+++ infra/homol.tf
@@ -118,6 +118,13 @@ resource "aws_security_group" "homol" {
     protocol     = "tcp"
     prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront[0].id]
   }
+  ingress {
+    description = "Web pública direta (easycob.duckdns.org)"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
